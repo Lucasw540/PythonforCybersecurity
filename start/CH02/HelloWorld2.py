78173666a35c94c41ff301a4ba1ba4112949a65b
lucas-whitney <whitney.lucas@student.edu>
2026-01-14 10:31:15 -0800

    Edits to hello Wrold 2

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created Lucas Whitney on 1/14/26
 
-# Suggestion, build out 1 line at a time
-# Once multiple print statemetns exist, put a breakpoint at first print line
-# Then walk through as an example of "debugging"
+# Ask user for their name
+user_name = input("What is your name? ")
+#Print hello and their name
+print("Hello{0}".format(user_name))
