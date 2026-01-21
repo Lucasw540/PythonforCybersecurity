ea7fb5cf8dca58274a1019550f6a542eb3415651
lucas-whitney <whitney.lucas@student.edu>
2026-01-21 11:25:41 -0800

    Loopy shit

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Lucas Whitney 1/21/26
+
+#prompt user to answer a question
+#Store in variable
+good_day_question = "Is today a good day (Y/n)"
+user_response = input(good_day_question)
+
+
+#if yes print a response
+if user_response == "y":
+    print("Yes it is")
+elif user_response =="Y":
+    print("That's amazing")
+else:
+    print("I hope it gets better.")
+
+
--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+#!/usr/bin/env python3
+# example workign with Functions
+#By Lucas Whitney 1/21/26
+
+#prompt user to answer a question
+#Store in variable
+good_day_question = "Is today a good day (Y/n)"
+user_response = input(good_day_question)
+
+
+#if yes print a response
+if user_response == "y":
+    for count in range(10):
+        print("Yes it is")
+elif user_response =="Y":
+    print("That's amazing")
+else:
+    print("I hope it gets better.")
+
